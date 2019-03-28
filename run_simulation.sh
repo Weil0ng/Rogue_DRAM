8f007669c5ed11ebe5dc434c519b12a72a7996cd
Weilong Cui <weil0ng@Yans.local>
2019-03-28 15:04:59 -0700

    Add tag, timing

--- run_simulation.sh
+++ run_simulation.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# $1: dramsim2_root, $2: config_dir $3: trace_path
+# $1: dramsim2_root, $2: config_dir $3: trace_path $4: result_tag
 echo "run DRAMSim2 for configs in $2"
-result_dir=$2/results/
+result_dir=$2/results_$4/
 mkdir -p $result_dir
 for config in $(find $2 -maxdepth 1 -type f);
 do
@@ -10,5 +10,5 @@ do
     echo "running "
     # -c -1: runs to end of trace
     # -q: only print last stats
-    $1/DRAMSim -t $3 -s $1/system.ini.example -d $config -c -1 -q > $result_dir/$base_name
+    time $1/DRAMSim -t $3 -s $1/system.ini.example -d $config -c -1 -q > $result_dir/$base_name
 done
